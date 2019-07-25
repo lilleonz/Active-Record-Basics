7b3a80af8e72f5119b1bc2d54b9ca16ceaa263e7
eapenzacharias <eapenzac@gmail.com>
2019-07-25 20:19:51 +0200

    added comment model and all associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     validates :username, presence:true ,uniqueness:true
     validates :email, presence:true, uniqueness:true
     validates :password, presence:true
+
+    has_many :posts
+    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_173814) do
+ActiveRecord::Schema.define(version: 2019_07_25_181224) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
